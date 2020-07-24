2a6e8e1d4314b10858d9fe0dbcc0ca05d524927c
Pierre Drouillet <pierrot@pop-os.localdomain>
2020-07-25 01:00:09 +0200

    menu reform

--- components/MenuList/MenuList.component.tsx
+++ components/MenuList/MenuList.component.tsx
@@ -1,79 +1,28 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 
-export default function MenuList() {
+import { Pizza } from '../../types'
+
+export interface MenuListProps {
+  pizzas: Pizza[]
+}
+
+export default function MenuList(props: MenuListProps) {
   return (
     <div className="menu">
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
-      <Typography>Pizzas</Typography>
+      <Typography variant="h4">Pizzas</Typography>
+      <Grid container spacing={3}>
+        {props.pizzas.map((pizza) => (
+          <Grid item xs={6}>
+            <Paper>
+              <Typography variant="h5">{pizza.name}</Typography>
+              <img src={pizza.image} />
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
--- components/MenuList/MenuList.container.tsx
+++ components/MenuList/MenuList.container.tsx
@@ -1,1 +1,7 @@
-export { default } from './MenuList.component'
+import { connect } from 'react-redux'
+import ReduxState from '../../redux/state'
+import MenuList from './MenuList.component'
+
+export default connect((state: ReduxState) => ({ pizzas: state.menu }))(
+  MenuList
+)
